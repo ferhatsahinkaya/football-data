a1a4ab7a53ed156841f33e12d089b4f765a4d82b
ferhat.sahinkaya <ferhat.sahinkaya@sky.uk>
2017-10-05 01:07:01 +0100

    Publish each competition to CompetitionTopic

--- src/publishCompetitions.py
+++ src/publishCompetitions.py
@@ -1,21 +1,19 @@
-# import boto3
+import boto3
 import json
 import requests
 
 def handler(event, context):
-    competitions = json.loads(requests.get('http://api.football-data.org/v1/competitions'))
+    sns_client = boto3.client('sns')
 
-    # client = boto3.client('sns')
+    response = requests.get('http://api.football-data.org/v1/competitions')
+    competitions = json.loads(response.content.decode('utf-8'))
 
     for competition in competitions:
-        print(competition)
-        # client.publish(
-        #     TopicArn = 'arn:aws:sns:eu-west-2:907683024242:CompetitionTopic',
-        #     Message = json.dumps({
-        #         'default' : '{}',
-        #         'competitions' : competition
-        #     }),
-        #     MessageStructure='json')
+        sns_client.publish(
+            TopicArn='arn:aws:sns:eu-west-2:907683024242:CompetitionTopic',
+            Message=json.dumps({
+                'default': json.dumps(competition)
+            }),
+            MessageStructure='json')
 
-if __name__ == "__publishCompetitions__":
-    handler(None, None)
+    return None
